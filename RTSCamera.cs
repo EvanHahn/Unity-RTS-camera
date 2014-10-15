cd0fa2eff15fd61d0d85c9c8c04f044689bbe51e
Evan Hahn <me@evanhahn.com>
2014-10-14 20:30:31 -0400

    We're drawing boxes

--- RTSCamera.cs
+++ RTSCamera.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 
 public class RTSCamera : MonoBehaviour {
-
-	private readonly string INPUT_MOUSE_X = "Mouse X";
-	private readonly string INPUT_MOUSE_Y = "Mouse Y";
+	
 	private readonly string[] INPUT_MOUSE_BUTTONS = {"Mouse Look", "Mouse Select"};
 	
 	private bool[] isDragging = new bool[2];
-	private Vector2[] dragOrigins = new Vector2[2];
-	private Vector3 lookOrigin;
+	private Vector3 selectStartPosition;
+	private Texture2D pixel;
+
+	void Start() {
+		setPixel(Color.green);
+	}
 	
 	void Update() {
 		updateDragging();
 		updateLook();
+	}
+
+	void OnGUI() {
 		updateSelect();
 	}
 
@@ -21,6 +26,9 @@ public class RTSCamera : MonoBehaviour {
 		for (int index = 0; index <= 1; index ++) {
 			if (isClicking(index) && !isDragging[index]) {
 				isDragging[index] = true;
+				if (index == 1) {
+					selectStartPosition = Input.mousePosition;
+				}
 			} else if (!isClicking(index) && isDragging[index]) {
 				isDragging[index] = false;
 			}
@@ -38,6 +46,7 @@ public class RTSCamera : MonoBehaviour {
 
 	private void updateSelect() {
 		if (!isDragging[1]) { return; }
+		drawRectangle(selectStartPosition, Input.mousePosition);
 	}
 
 	private bool isClicking(int index) {
@@ -45,7 +54,24 @@ public class RTSCamera : MonoBehaviour {
 	}
 
 	private Vector2 getMouseMovement() {
-		return new Vector2(Input.GetAxis(INPUT_MOUSE_X), Input.GetAxis(INPUT_MOUSE_Y));
+		return new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+	}
+
+	private void drawRectangle(Vector3 cornerA, Vector3 cornerB) {
+		var x = cornerA.x;
+		var y = cornerB.y;
+		var width = cornerB.x - cornerA.x;
+		var height = cornerB.y - cornerA.y;
+		GUI.DrawTexture(new Rect(x, y, width, 1), pixel);
+		GUI.DrawTexture(new Rect(x, y, 1, height), pixel);
+		GUI.DrawTexture(new Rect(x, y + height, width, 1), pixel);
+		GUI.DrawTexture(new Rect(x + width, y, 1, height), pixel);
+	}
+
+	private void setPixel(Color color) {
+		pixel = new Texture2D(1, 1);
+		pixel.SetPixel(0, 0, color);
+		pixel.Apply();
 	}
 
 }
