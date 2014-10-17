a42624f88ff693e30ba657a5d1e168fa695f9c33
Evan Hahn <me@evanhahn.com>
2014-10-17 08:05:06 -0700

    Add license to C#

--- Demo/Assets/RTSCamera.cs
+++ Demo/Assets/RTSCamera.cs
@@ -1,3 +1,31 @@
+/*
+
+RTS camera for Unity
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Evan Hahn
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -14,7 +42,7 @@ public class RTSCamera : MonoBehaviour {
 
 	public float lookDamper = 5f;
 	public string selectionObjectName = "RTS Selection";
-	
+
 	private readonly string[] INPUT_MOUSE_BUTTONS = {"Mouse Look", "Mouse Select"};
 	private bool ready;
 	private bool[] isDragging = new bool[2];
@@ -34,7 +62,7 @@ public class RTSCamera : MonoBehaviour {
 		setPixel(selectColor);
 		debugSelection = true;
 	}
-	
+
 	void Update() {
 		if (!ready) { return; }
 		updateDragging();
